134d7c99e0171e5fbaeea3b37ed76e67848484b9
Fayis2021 <fayisatgit@gmail.com>
2024-05-30 17:16:18 +0530

    roll new

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,22 @@ import DiceThree from '../assets/Three.png';
 import DiceFour from '../assets/Four.png';
 import DiceFive from '../assets/Five.png';
 import DiceSix from '../assets/Six.png';
+import consstantsM from './constants';
+
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+
+
+
 
 type Diceprops = PropsWithChildren<{
   imageurl: ImageSourcePropType;
 }>;
+
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
+
 
 const Dice = ({imageurl}: Diceprops): JSX.Element => {
   return (
@@ -30,6 +42,7 @@ const Dice = ({imageurl}: Diceprops): JSX.Element => {
 
 function App(): JSX.Element {
   const [imagemine, setChangeImage] = useState<ImageSourcePropType>(DiceOne);
+  const [imagemine2, setChangeImage2] = useState<ImageSourcePropType>(DiceOne);
 
   const changeDice = () => {
     let randonNum = Math.floor(Math.random() * 6) + 1;
@@ -54,17 +67,54 @@ function App(): JSX.Element {
         break;
       default:
     }
+    ReactNativeHapticFeedback.trigger("impactLight", options);
+    changeDice2()
+  };
+  const changeDice2 = () => {
+    let randonNum = Math.floor(Math.random() * 6) + 1;
+    switch (randonNum) {
+      case 1:
+        setChangeImage2(DiceOne);
+        break;
+      case 2:
+        setChangeImage2(DiceTwo);
+        break;
+      case 3:
+        setChangeImage2(DiceThree);
+        break;
+      case 4:
+        setChangeImage2(DiceFour);
+        break;
+      case 5:
+        setChangeImage2(DiceFive);
+        break;
+      case 6:
+        setChangeImage2(DiceSix);
+        break;
+      default:
+    }
+   
   };
 
   return (
     <View style={styles.mainscreen}>
       <TouchableOpacity onPress={changeDice}>
+        <Dice imageurl={imagemine2} />
         <Dice imageurl={imagemine} />
       </TouchableOpacity>
       <Pressable onPress={changeDice} style={styles.pressable}>
-        <Text>Roll the Dice</Text>
+        <Text>
+          Hi
+        </Text>
         </Pressable>
-
+        <Pressable onPress={changeDice} style={styles.pressable}>
+        <Text>
+          {consstantsM.Cancel}
+        </Text>
+        <Text>
+          {consstantsM.Proceed}
+        </Text>
+        </Pressable>
     </View>
   );
 }
---
+++ src/constants.ts
@@ -0,0 +1,26 @@
+
+
+
+// import { Text, View } from 'react-native';
+
+import { Text } from "react-native"
+
+// const ButtonOne=()=>{
+// return(
+//   <View style={ {borderColor:"blue",  borderWidth: 2,  borderRadius: 8,   paddingVertical: 10,
+//   paddingHorizontal: 40,}}>
+// <Text >
+//   roll the dice
+// </Text>
+//   </View>
+// );
+// }
+//  export default ButtonOne
+// constants/colors.js
+
+const consstantsM={
+   Cancel:"cancel",
+   Proceed:"Proceed",
+}
+
+export default consstantsM
